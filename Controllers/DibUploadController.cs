24a8e9cfff37b18cee2b1c612febf6c701abd994
Darek <mrocznaklawiatura@gmail.com>
2019-09-18 20:46:06 +0200

    version control B))))))))))

--- Controllers/DibUploadController.cs
+++ Controllers/DibUploadController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using web2.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,27 +30,32 @@ namespace web2.Controllers
             if (formFile.Length == 0)
                 return;
             
-            string filePath = Path.GetFileNameWithoutExtension(formFile.FileName) + "/" + formFile.FileName;
-            string directoryPath = Path.GetDirectoryName(filePath);
+            string appName = Path.GetFileNameWithoutExtension(formFile.FileName);
+            AppDirectoryData appDirectoryData = new AppDirectoryData(appName);
+            string filePath = appDirectoryData.GetAppDirectoryPath() + "/" + formFile.FileName;
+            string dedicatedVersionRepoPath = appDirectoryData.GetCurrentVersionRepoPath();
+            string masterRepoPath = appDirectoryData.GetMasterRepoPath();
 
-            createDirectoryIfDoesntExist(directoryPath);
-            clearDirectory(directoryPath);
+            createRepo(dedicatedVersionRepoPath);
             saveUploadedRepoZip(formFile, filePath);
-            extractRepoZip(filePath, directoryPath);
+            extractRepoZip(filePath, dedicatedVersionRepoPath);
+            clearRepo(masterRepoPath);
+            extractRepoZip(filePath, masterRepoPath);
             removeCompressedFile(filePath);
+
+            appDirectoryData.IncreaseCurrentVersion();
         }
 
-        void createDirectoryIfDoesntExist(string directoryPath)
+        void createRepo(string repoPath)
         {
-            if (!Directory.Exists(directoryPath))
-                Directory.CreateDirectory(directoryPath);
+            Directory.CreateDirectory(repoPath);
         }
 
-        void clearDirectory(string consideredDirectoryPath)
+        void clearRepo(string repoPath)
         {
-            foreach (string filePath in Directory.GetFiles(consideredDirectoryPath))
+            foreach (string filePath in Directory.GetFiles(repoPath))
                 System.IO.File.Delete(filePath);
-            foreach (string directoryPath in Directory.GetDirectories(consideredDirectoryPath))
+            foreach (string directoryPath in Directory.GetDirectories(repoPath))
                 Directory.Delete(directoryPath, true);
         }
 
---
+++ Data/AppDirectoryData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace web2.Data
+{
+    class AppDirectoryData
+    {
+        const string MASTER_REPO_ID = "master";
+        const string PATH_TO_APP_DIRECTORIES = "AppRepos";
+        static Dictionary<string, int> currentVersions = new Dictionary<string, int>();
+
+        string appName;
+
+        static AppDirectoryData()
+        {
+            if (!Directory.Exists(PATH_TO_APP_DIRECTORIES))
+                Directory.CreateDirectory(PATH_TO_APP_DIRECTORIES);
+        }
+
+        public AppDirectoryData(string appName)
+        {
+            this.appName = appName;
+            if (!currentVersions.ContainsKey(appName))
+            {
+                currentVersions.Add(appName, 0);
+                Directory.CreateDirectory(GetAppDirectoryPath());
+                Directory.CreateDirectory(GetMasterRepoPath());
+            }
+        }
+
+        public void IncreaseCurrentVersion()
+        {
+            currentVersions[appName]++;
+        }
+
+        public string GetAppDirectoryPath()
+        {
+            return $"{PATH_TO_APP_DIRECTORIES}/{appName}";
+        }
+
+        public string GetCurrentVersionRepoPath()
+        {
+            return $"{PATH_TO_APP_DIRECTORIES}/{appName}/{currentVersions[appName]}";
+        }
+
+        public string GetMasterRepoPath()
+        {
+            return $"{PATH_TO_APP_DIRECTORIES}/{appName}/{MASTER_REPO_ID}";
+        }
+    }
+}
