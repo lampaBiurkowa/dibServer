4f680d55adfe3f7160ae0f329d96587674069c05
Darek <mrocznaklawiatura@gmail.com>
2019-09-18 18:54:05 +0200

    initial commit

---
+++ Controllers/DibUploadController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace web2.Controllers
+{
+    [Route("api/[controller]")]
+    public class DibUploadController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (IFormFile formFile in files)
+                handleSingleFileUpload(formFile);
+
+            return Ok(new { count = files.Count, size });
+        }
+
+        void handleSingleFileUpload(IFormFile formFile)
+        {
+            if (formFile.Length == 0)
+                return;
+            
+            string filePath = Path.GetFileNameWithoutExtension(formFile.FileName) + "/" + formFile.FileName;
+
+            System.Console.WriteLine(filePath);
+            using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                formFile.CopyTo(stream);
+        }
+
+        void createDirectoryIfDoesntExist(string filePath)
+        {
+            string directoryPath = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
